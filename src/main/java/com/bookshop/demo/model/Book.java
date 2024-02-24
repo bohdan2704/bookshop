6177d652fa35642f6009bce5035dab7910e510b3
Bohdan <bohdan2704@gmail.com>
2024-02-24 18:58:40 +0200

    fixed checkstyle

--- src/main/java/com/bookshop/demo/model/Book.java
+++ src/main/java/com/bookshop/demo/model/Book.java
@@ -1,9 +1,12 @@
 package com.bookshop.demo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.Data;
 
 @Entity
 @Data
